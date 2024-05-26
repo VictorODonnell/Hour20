cd38053a478d7e48fb965ceeaf811ab5be227e2b
victor Odonnell <39573414+vipereaper@users.noreply.github.com>
2024-05-25 17:01:53 -0700

    Hour20

---
+++ Vodonnell_hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,25 @@
+// ,ad8888ba,                                             ,ad8888ba,             88         88             
+// d8"'    `"8b                                           d8"'    `"8b            ""   ,d    ""             
+//d8'                                                    d8'                           88                   
+//88            ,adPPYYba, 88,dPYba,,adPYba,   ,adPPYba, 88            8b,dPPYba, 88 MM88MMM 88  ,adPPYba,  
+//88      88888 ""     `Y8 88P'   "88"    "8a a8P_____88 88            88P'   "Y8 88   88    88 a8"     ""  
+//Y8,        88 ,adPPPPP88 88      88      88 8PP""""""" Y8,           88         88   88    88 8b          
+// Y8a.    .a88 88,    ,88 88      88      88 "8b,   ,aa  Y8a.    .a8P 88         88   88,   88 "8a,   ,aa  
+//  `"Y88888P"  `"8bbdP"Y8 88      88      88  `"Ybbd8"'   `"Y8888Y"'  88         88   "Y888 88  `"Ybbd8"'  
+
+//This game might be too easy because its easier to collect the powerupps so you will pick up many more powerups than obstacles
+//This game has an issue where you can spam the spacebar to use your phase ability
+
+//I want to add a way so the obstacles subtract more time the longer you keep going 
+//I would like to add a cool down effect so it should only be used wisely
+
+//One wow factor would be the unique mechanic of phasing through obstacles 
+//Another wow factor that would noted is the endless runner when you're only running in place to avoid loading lag 
+
+//I personally enjoy the fact that this game can go on endlessly
+//I also enjoy the addition to powerups adding time and obstacles taking away time
+
+//I can see the tediousness of it being the same randomness each time you restart
+//Possibly another tedious part of the game is the lack of style in the background. Other than that, im not entirely sure
+
+//I went ahead and changed the obstacle to a donut and changed the particle system on the powerup to be circling around the object
